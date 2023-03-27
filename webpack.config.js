1b4cd89a0da0d6938a1f186d0550ad3c96d5165e
Zhitao Chen <zhitao.chen@rtl.de>
2023-03-27 16:13:05 +0200

    Ex 2: Webpack Entry - Workspace

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const webpack = require('webpack')
 
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
+
 module.exports = {
     entry: './src/client/index.js',
 }
